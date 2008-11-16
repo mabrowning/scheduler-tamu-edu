df06def4c1280acfa36024c35a6066118bc1c035
Mark Browning <mabrowningrr@gmail.com>
2008-11-16 22:48:34 +0000

    added other backend tools

---
+++ parser/getcourses.py
@@ -0,0 +1,151 @@
+#! /usr/bin/python
+
+import urllib
+from HTMLParser import HTMLParser
+import pickle
+
+class CoursesHTMLParser(HTMLParser):
+	"This handles HTML pages from courses.tamu.edu and extracts the desired link payloads"
+	def __init__(self,target="viewcourses"):
+		HTMLParser.__init__(self)
+		self.target=target
+		self.flag=False
+		self.payload={}
+	def handle_starttag(self, tag, attrs):
+		if(tag=="a"):
+			if(attrs[0][1].capitalize().find(self.target.capitalize())>-1):
+				self.flag=True
+				self.tempx=attrs[0][1].rpartition("&")[0].rpartition("=")[2]
+	def handle_data(self, data):
+		if(self.flag):
+			self.payload[self.tempx]=data.split(" ")[2]
+	def handle_endtag(self, tag):
+		if(self.flag):
+			self.flag=False
+class SectionHTMLParser(HTMLParser):
+	"This handles HTML pages from courses.tamu.edu and extracts the section attributes"
+	def __init__(self):
+		HTMLParser.__init__(self)
+		self.sections=[]
+		self.temp={}
+		self.temp["tdr"]=[]
+		self.temp["notes"]=""
+		self.tagflag=""
+		self.strongflag=""
+		self.secnum="0"
+		self.donenotes=False
+	def handle_starttag(self, tag, attrs):
+		args=self.handle_args(attrs)
+		if(tag=="td" and "class" in args and args["class"]=="sectionheading"):
+			if(self.secnum!="0"):
+				self.sections.append(self.temp)
+				self.temp={}
+				self.temp["tdr"]=[]
+				self.temp["notes"]=""
+				self.donenotes=False
+			self.strongflag="description"
+		if(tag=="strong"):
+			self.tagflag="strong"
+	def handle_data(self, data):
+		if self.strongflag == "credit":
+			self.temp[self.strongflag]=data.strip("CR \r\n")
+			self.strongflag=""
+		elif self.strongflag=="description":
+			ds=data.split("-")[1:]
+			data=""
+			for i in ds:
+				data+=i.strip(" \r\n")+" - "
+			self.secnum=data.split(" ")[0].strip()
+			self.temp["section"]=self.secnum
+			self.temp["description"]=data.strip("0123456789- \r\n")
+			self.strongflag=""
+		elif self.strongflag=="tdr":
+			tdr=data.split(";")[-1:][0].strip(" \t\r\n")
+			if tdr!="":
+				self.temp["tdr"].append(tdr)
+		elif self.strongflag != "":
+			self.temp[self.strongflag]=data.strip(" \r\n")
+			self.strongflag=""
+		
+		if(self.tagflag=="strong"):
+			if data=="Instructor:":
+				self.strongflag="prof"
+			elif data=="Total Seats:":
+				self.strongflag="totalseats"
+			elif data=="Available Seats":
+				self.strongflag="availseats"
+			elif data=="Credits:":
+				self.strongflag="credit"
+			elif data=="Locations:":
+				self.strongflag="tdr"
+		
+		self.tagflag="None"
+	def handle_endtag(self, tag):
+		if (tag=="td" and self.strongflag=="tdr"):
+			self.strongflag=""
+		if tag=="table":
+			self.sections.append(self.temp)
+		self.tagflag=="None"
+	def handle_args(self, attrs):
+		args={}
+		for i in attrs:
+			args[i[0]]=i[1]
+		return args
+def getdepts(x):
+	"This returns a dictionary of DEPT:base64_codes pairs corresponding the department list"
+	
+	dept=CoursesHTMLParser("viewcourses")
+	dept.feed(urllib.urlopen("http://courses.tamu.edu/viewdepartments.aspx?x="+x).read())
+	return dept.payload
+def getcourses(x):
+	"This returns a dictionary of DEPT:base64_codes pairs corresponding the course list in a particular department"
+	
+	dept=CoursesHTMLParser("viewsections")
+	dept.feed(urllib.urlopen("http://courses.tamu.edu/viewcourses.aspx?x="+x).read())
+	return dept.payload
+def getsects(x):
+	"This returns a dictionary containing all the sections and their attributes in a course"
+
+	section=SectionHTMLParser()
+	section.feed(urllib.urlopen("http://courses.tamu.edu/viewsections_pf.aspx?x="+x).read().replace("&","AND").replace("</A<","</A><").replace("<A <","<"))
+	# The replace needs to be done because HTMLParser stops a 'handle_data()' response at an '&'
+	# and some course descriptions have '&' in them
+	return section.sections
+
+print "Getting the department list..."
+#depts=getdepts("K22FPSHrTqwkOV9VFP/yQw==")
+depts=getdepts("bqPHRw6nsa27Rp2YPdL5qw==")
+print depts
+courses={}
+sects={}
+# in the following code, de and co are the Base64 keys. 
+# depts[de] contains the strings corresponding to the keys
+# courses[de][co] contains the course number in department with key 'de' associated with key 'co'
+
+# sects[de][co] contains not a string, but an array of associatiave arrays with the following fields
+# ["section"]     -> section number
+# ["description"] -> course description
+# ["totalseats"]  -> total seats in section
+# ["availseats"]  -> available seats in the section
+# ["prof"]   -> the instructor teaching it. duh ;)
+# ["credit"] -> string containing the number of credits; format is "%d"
+# ["tdr"]    -> a simple array of strings containing the time, day, room strings
+# ["notes"]  -> empty;(TODO) should contain notes about the section (like, is it honors?, et c)
+
+
+for de in depts:
+	print depts[de]
+	sects[de]={}
+	courses[de]=getcourses(de)
+	for co in courses[de]:
+		print "   "+courses[de][co]
+		sects[de][co]=getsects(co)
+		
+# save this shit! It takes a long time to download, what if we crash in the next step?
+f = open("courses.pkl","wb")
+pickle.dump(sects, f)
+pickle.dump(depts, f)
+pickle.dump(courses, f)
+f.close()
+print "saved everything to courses.pkl!  Make sure to run ./interpret.py"
+
---
+++ parser/interpret.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+
+import pickle
+import MySQLdb
+
+db = MySQLdb.connect(user="course",passwd="shoot",db="2009A")
+cur = db.cursor()
+
+def create(dept,course):
+	cur.execute("CREATE TABLE IF NOT EXISTS "+dept+course+" ( section TEXT, prof TEXT, descrip TEXT, credit TEXT, seats TEXT, seatsleft TEXT, TDR0 TEXT, TDR1 TEXT, TDR2 TEXT, TDR3 TEXT, TDR4 TEXT);")
+	cur.execute("TRUNCATE TABLE "+dept+course+";")
+def append(dept,course,section):
+	sql="INSERT INTO "+dept+course+" ( section,prof,descrip,credit,seats,seatsleft"
+	for i in range(len(section["tdr"])):
+		sql+=",TDR"+str(i)
+	sql+=" ) VALUES ('"
+	sql+=section["section"]+"','"
+	sql+=MySQLdb.escape_string(section["prof"])+"','"
+	sql+=MySQLdb.escape_string(section["description"])+"','"
+	sql+=section["credit"]+"','"
+	sql+=section["totalseats"]+"','"
+	sql+=section["availseats"]
+	for i in section["tdr"]:
+		sql+="','"+i	
+	sql+="');"
+	cur.execute(sql)
+
+f = open("courses.pkl","rb")
+sects=pickle.load(f)
+depts=pickle.load(f)
+courses=pickle.load(f)
+f.close()
+print "updating database structure"
+for de in sects:
+	for co in sects[de]:
+		create(depts[de],courses[de][co])
+print "updating database contents"
+for de in sects:
+	for co in sects[de]:
+		for se in sects[de][co]:
+			append(depts[de],courses[de][co],se)
+
+
+	

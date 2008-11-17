044c9dbdfe3a0d97b47cf529e4b64b9161103d0e
Mark Browning <mabrowningrr@gmail.com>
2008-11-17 05:29:45 +0000

    database import now works properly

--- parser/initial.py
+++ parser/initial.py
@@ -5,35 +5,58 @@ import MySQLdb
 
 db = MySQLdb.connect(user="course",passwd="shoot",db="2009A")
 cur = db.cursor()
-
-def append(dept,course,section):
-	sql="INSERT INTO "+dept+course+" ( section,prof,descrip,credit,seats,seatsleft"
+def create_dept(dept):
+	cur.execute("INSERT INTO departments (abbreviation) VALUES ('"+dept+"');")
+	cur.execute("SELECT LAST_INSERT_ID();")
+	return (dept,cur.fetchone()[0])
+def create_course(dept,course):
+	cur.execute("INSERT INTO courses (department_id, number) VALUES ("+str(dept)+","+course+");")
+	cur.execute("SELECT LAST_INSERT_ID();")
+	return (course,cur.fetchone()[0])
+def create_prof(dept,prof):
+	cur.execute("INSERT INTO profs (department_id, display_name) VALUES ("+str(dept)+", '"+MySQLdb.escape_string(prof)+"');")
+	cur.execute("SELECT LAST_INSERT_ID();")
+	return cur.fetchone()[0]
+def create_section(course,section):
+	sql="INSERT INTO sections (course_id, prof_id, number, seats, seats_avail, credit, honors, writing_intensive, description_string, notes"
 	for i in range(len(section["tdr"])):
-		sql+=",TDR"+str(i)
-	sql+=" ) VALUES ('"
-	sql+=section["section"]+"','"
-	sql+=MySQLdb.escape_string(section["prof"])+"','"
-	sql+=MySQLdb.escape_string(section["description"])+"','"
-	sql+=section["credit"]+"','"
-	sql+=section["totalseats"]+"','"
-	sql+=section["availseats"]
+		sql+=", TDR"+str(i)
+	sql+=" ) VALUES ("+str(course)+", "+str(section["prof"])+", "+section["section"]+", "+section["totalseats"]+", "+section["availseats"]+", "
+	sql+=section["credit"].split(' ')[0]+", " 
+	if (section["section"]>=200 and section["section"]<300) or section["section"]==970:
+		sql+="1, "
+	else:
+		sql+="0, "
+	if section["section"]>=900:
+		sql+="1, "
+	else:
+		sql+="0, "
+	sql+="'"+MySQLdb.escape_string(section["description"])+"', '"+MySQLdb.escape_string(section["notes"])
 	for i in section["tdr"]:
-		sql+="','"+i	
+		sql+="', '"+i
 	sql+="');"
 	cur.execute(sql)
-
 f = open("courses.pkl","rb")
 sects=pickle.load(f)
 depts=pickle.load(f)
 courses=pickle.load(f)
 f.close()
-print "adding fresh depts, profs, courses and sections to empty database"
-
+print "adding departments..."
+for de in depts:
+	depts[de]=create_dept(depts[de])
+print "adding courses..."
+for de in courses:
+	for co in courses[de]:
+		courses[de][co]=create_course(depts[de][1],courses[de][co])
+print "adding profs and sections..."
+profs={}
 for de in sects:
-	for co in sects[de]:
-		create(depts[de],courses[de][co])
-print "updating database contents"
-for de in sects:
+	profs[de]={}
 	for co in sects[de]:
 		for se in sects[de][co]:
-			append(depts[de],courses[de][co],se)
+			if(se["prof"] in profs[de]):
+				se["prof"]=profs[de][se["prof"]]
+			else:
+				profs[de][se["prof"]]=create_prof(depts[de][1],se["prof"])
+				se["prof"]=profs[de][se["prof"]]
+			create_section(courses[de][co][1],se)

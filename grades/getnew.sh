91210e3be645846751aff882e2230a542f1dac71
Mark Browning <mabrowningrr@gmail.com>
2008-11-17 17:58:06 +0000

    actually added the new parser... updated the getnew.sh

--- grades/getnew.sh
+++ grades/getnew.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 #
-MYSQL_user=course
-MYSQL_password=shoot
-MYSQL_host=localhost
 sem=$1
 [ -z $1 ] && echo "usage: getnew.sh semester Example: ./getnew.sh 2007C"
 echo "are you SURE you want to download and add the grades from $1 to the database?"
@@ -13,13 +10,11 @@ do
 	#This hardcodes admissions.tamu.edu becuase my DNS server was slow
 	wget "http://165.91.18.172/Registrar/FacultyStaff/Report/TXT_GRDDist/$sem/$i$sem.txt"
 done
-rm sql
 for filename in *.txt
 do
 	sed -e '/^[A-Z]/!d' -e '/DEPA.*/d' -e '/UNIV.*/d' < $filename >$filename.new
-	gawk -f sql.awk -v SEM=$sem $filename.new >>sql
+	./update.py < $filename.new
 done
 mv *.txt old/
 mv *.new parsed/
-mysql --user=$MYSQL_user --password=$MYSQL_password --host=$MYSQL_host -D TAMUGRADES < sql
 
---
+++ grades/update.py
@@ -0,0 +1,51 @@
+#! /usr/bin/python
+
+import MySQLdb
+import sys
+
+db = MySQLdb.connect(user="course",passwd="shoot",db="schedule")
+cur = db.cursor()
+
+def get_dept_id(dept):
+	cur.execute("SELECT id FROM departments WHERE dept='"+dept+"';")
+	return str(cur.fetchone()[0])
+def get_course_id(dept,course):
+	cur.execute("SELECT courses.id FROM departments,courses WHERE number="+course+" AND dept='"+dept+"' AND department_id=departments.id;")
+	return str(cur.fetchone()[0])
+def get_prof_id(dept,prof):
+	cur.execute("SELECT id FROM profs WHERE department_id='"+dept+"' AND display_name='"+MySQLdb.escape_string(prof)+"';")	
+	id=cur.fetchone()
+	if(id):
+		return id[0]
+	else:
+		cur.execute("INSERT INTO profs (department_id,display_name) VALUES( "+dept+", '"+MySQLdb.escape_string(prof)+"');")
+		cur.execute("SELECT LAST_INSERT_ID();")
+		return cur.fetchone()[0]
+def insert_grade(department_id,prof_id,course_number,section_number,A,B,C,D,F,TOT):
+	cur.execute("INSERT INTO grades (department_id,prof_id,course_number,section_number,A,B,C,D,F,TOT) VALUES "+\
+		"("+department_id+", "+prof_id+", "+course_number+", "+section_number+", "+A+", "+B+", "+C+", "+D+", "+F+", "+TOT+");")
+fields={}
+fields["department_id"]=(0,4)
+fields["course_number"]=(5,3)
+fields["section_number"]=(9,3)
+fields["A"]=(18,7)
+fields["B"]=(26,7)
+fields["C"]=(34,7)
+fields["D"]=(42,7)
+fields["F"]=(50,7)
+fields["TOT"]=(58,6)
+fields["prof_id"]=(108,30)
+for line in sys.stdin:
+	line=line.strip(" \r\n")
+	values={}
+	for f in fields:
+		values[f]=line[fields[f][0]:sum(fields[f])]
+		if(values[f].strip(" ")==''):
+			values[f]='0'
+	values["department_id"]=get_dept_id(values["department_id"])
+	values["prof_id"]=get_prof_id(values["department_id"],values["prof_id"])
+	for f in fields:
+			values[f]=str(int(values[f]))
+	if(values['TOT']=='0'):
+		continue
+	insert_grade(**values)

0a05387653b395c3f45e5f9871cd3af476807609
Mark Browning <mabrowningrr@gmail.com>
2008-11-18 04:09:43 +0000

    keeping things up to date

--- grades/update.py
+++ grades/update.py
@@ -8,17 +8,26 @@ cur = db.cursor()
 
 def get_dept_id(dept):
 	cur.execute("SELECT id FROM departments WHERE dept='"+dept+"';")
-	return str(cur.fetchone()[0])
-def get_course_id(dept,course):
-	cur.execute("SELECT courses.id FROM departments,courses WHERE number="+course+" AND dept='"+dept+"' AND department_id=departments.id;")
-	return str(cur.fetchone()[0])
-def get_prof_id(dept,prof):
-	cur.execute("SELECT id FROM profs WHERE department_id='"+dept+"' AND display_name='"+MySQLdb.escape_string(prof)+"';")	
 	id=cur.fetchone()
 	if(id):
 		return id[0]
 	else:
-		cur.execute("INSERT INTO profs (department_id,display_name) VALUES( "+dept+", '"+MySQLdb.escape_string(prof)+"');")
+		cur.execute("INSERT INTO departments (dept) VALUES( '"+dept+"' );")
+		cur.execute("SELECT LAST_INSERT_ID();")
+		id=cur.fetchone()[0]
+		print "Adding new department: "+dept+" with id: "+str(id)
+		return id
+def get_course_id(dept,course):
+	cur.execute("SELECT courses.id FROM departments,courses WHERE number="+course+" AND dept='"+dept+"' AND department_id=departments.id;")
+	return str(cur.fetchone()[0])
+def get_prof_id(dept,prof):
+	cur.execute("SELECT id FROM profs WHERE department_id='"+str(dept)+"' AND display_name='"+MySQLdb.escape_string(prof)+"';")	
+	id=cur.fetchone()
+	if(id):
+		return id[0]
+	else:
+		print "Adding new prof: "+prof+" to department_id: "+str(dept)
+		cur.execute("INSERT INTO profs (department_id,display_name) VALUES( "+str(dept)+", '"+MySQLdb.escape_string(prof)+"');")
 		cur.execute("SELECT LAST_INSERT_ID();")
 		return cur.fetchone()[0]
 def insert_grade(department_id,prof_id,course_number,section_number,A,B,C,D,F,TOT):
@@ -42,6 +51,8 @@ for line in sys.stdin:
 		values[f]=line[fields[f][0]:sum(fields[f])]
 		if(values[f].strip(" ")==''):
 			values[f]='0'
+	if(values["prof_id"]=='0'):
+		continue
 	values["department_id"]=get_dept_id(values["department_id"])
 	values["prof_id"]=get_prof_id(values["department_id"],values["prof_id"])
 	for f in fields:

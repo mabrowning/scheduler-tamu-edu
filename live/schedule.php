7237d74b1dfe95256d4d01c8f8d903fe91e68918
Mark Browning <mabrowningrr@gmail.com>
2008-11-18 20:07:10 +0000

    Migrating new dynamic sizing into main program - attempt 1

--- live/schedule.php
+++ live/schedule.php
@@ -8,30 +8,7 @@
 </head>
 <body style='margin-top:5px;' onload="Init();">
 <h3>Spring 2009 Schedule Planner -- Development</h3>
-<div id='mediumcontianer'>
-<div id='times'>
-<?php 
-$start_hour=8;
-$last_hour=19;
-for($i=8;$i<20;$i++)
-{
-	$j=($i%12);
-	if($i==0||$i==12)$j="12";
-	if($i>11)
-		$j=$j.":00PM";
-	else
-		$j=$j.":00AM";
-	echo "<div class='hour' id='".$i."'><span class='hourlabel'>".$j."</span></div>\n";
-	echo "<div class='halfhour'></div>\n";
-}?>
-</div>
-<div id='cal'>
-<div id='M' class='day'></div>
-<div id='T' class='day'></div>
-<div id='W' class='day'></div>
-<div id='R' class='day'></div>
-<div id='F' class='day'></div>
-</div><!-- cal -->
+<div id='schedulecontianer'>
 </div><!-- mediumcontainer -->
 <div id='container'>
 	<div id='courseselector'>

9ad24aa8c46d241729cc0d959b523a68432c1f99
Mark Browning <mabrowningrr@gmail.com>
2008-11-22 04:58:39 +0000

    updated databse structure

--- database/dbcreate.sql
+++ database/dbcreate.sql
@@ -2,16 +2,14 @@ CREATE TABLE IF NOT EXISTS departments(
   id INT(3) AUTO_INCREMENT PRIMARY KEY,
   dept CHAR(4) NOT NULL
 );
-TRUNCATE TABLE departments;
 
-CREATE TABLE IF NOT EXISTS courses(
-  id INT(5) AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS allcourses(
+  id INT(7) AUTO_INCREMENT PRIMARY KEY,
   department_id INT(3) NOT NULL,
   number INT(3) NOT NULL,
   description_string VARCHAR(1023),
   FOREIGN KEY (department_id) REFERENCES departments (id)
 );
-TRUNCATE TABLE courses;
 
 CREATE TABLE IF NOT EXISTS profs(
   id INT(5) AUTO_INCREMENT PRIMARY KEY,
@@ -19,12 +17,36 @@ CREATE TABLE IF NOT EXISTS profs(
   display_name VARCHAR(25) NOT NULL,
   FOREIGN KEY (department_id) REFERENCES departments (id)
 );
-TRUNCATE TABLE profs;
+
+CREATE TABLE IF NOT EXISTS grades(
+  id INT(7) AUTO_INCREMENT PRIMARY KEY,
+  department_id INT(3) NOT NULL,
+  prof_id INT(5) NOT NULL,
+  course_id INT(7) NOT NULL,
+  section_number INT(3) NOT NULL,
+  A INT(3) NOT NULL,
+  B INT(3) NOT NULL,
+  C INT(3) NOT NULL,
+  D INT(3) NOT NULL,
+  F INT(3) NOT NULL,
+  TOT INT(3) NOT NULL
+  FOREIGN KEY (department_id) REFERENCES departments (id)
+  FOREIGN KEY (prof_id) REFERENCES profs (id)
+  FOREIGN KEY (course_id) REFERENCES allcourses (id)
+);
+
+CREATE TABLE IF NOT EXISTS courses(
+  id INT(5) AUTO_INCREMENT PRIMARY KEY,
+  course_id INT(7) NOT NULL,
+  semester CHAR(5) NOT NULL,
+  FOREIGN KEY (course_id) REFERENCES allcourses (id)
+);
 
 CREATE TABLE IF NOT EXISTS sections(
   id INT(6) AUTO_INCREMENT PRIMARY KEY,
   course_id INT(5) NOT NULL,
   prof_id INT(5) NOT NULL,
+  semester CHAR(5) NOT NULL,
   number INT(3) NOT NULL,
   TDR0 VARCHAR(50) NOT NULL,
   TDR1 VARCHAR(50),
@@ -40,19 +62,4 @@ CREATE TABLE IF NOT EXISTS sections(
   credit INT(1) NOT NULL,
   FOREIGN KEY (course_id) REFERENCES courses (id),
   FOREIGN KEY (prof_id) REFERENCES profs (id)
-);
-TRUNCATE TABLE sections;
-
-CREATE TABLE IF NOT EXISTS grades(
-  id INT(7) AUTO_INCREMENT PRIMARY KEY,
-  department_id INT(3) NOT NULL,
-  prof_id INT(5) NOT NULL,
-  course_number INT(3) NOT NULL,
-  section_number INT(3) NOT NULL,
-  A INT(3) NOT NULL,
-  B INT(3) NOT NULL,
-  C INT(3) NOT NULL,
-  D INT(3) NOT NULL,
-  F INT(3) NOT NULL,
-  TOT INT(3) NOT NULL
 );

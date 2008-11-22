8e4562ca2ea0bddb174375fa942fc00e6c545a73
Mark Browning <mabrowningrr@gmail.com>
2008-11-22 08:58:03 +0000

    AND AND is not valid SQL

--- live/getcourse.php
+++ live/getcourse.php
@@ -13,7 +13,7 @@ $JSON=Array();
 $sql="SELECT sections.number, TDR0, TDR1, TDR2, TDR3, TDR4, seats, seats_available, sections.description_string, notes, credit, display_name ";
 $sql.="FROM sections,courses,allcourses,departments,profs WHERE allcourses.number=".$number." AND dept='".$dept."' AND ";
 $sql.="sections.semester='".$semester."' AND sections.course_id=courses.id AND courses.course_id=allcourses.id AND ";
-$sql.="AND allcourses.department_id=departments.id AND profs.department_id=departments.id AND prof_id=profs.id";
+$sql.="allcourses.department_id=departments.id AND profs.department_id=departments.id AND prof_id=profs.id";
 print $sql;
 $result=mysql_query($sql)or die('Query failed: ' . mysql_error());
 while($JSON[]=mysql_fetch_array($result,MYSQL_ASSOC));

97f592654cbe9ef85745edb452fe9cad77ecd38d
Mark Browning <mabrowningrr@gmail.com>
2008-11-22 05:19:37 +0000

    migradted grade parser to new database structure

--- grades/update.py
+++ grades/update.py
@@ -18,8 +18,16 @@ def get_dept_id(dept):
 		print "Adding new department: "+dept+" with id: "+str(id)
 		return id
 def get_course_id(dept,course):
-	cur.execute("SELECT courses.id FROM departments,courses WHERE number="+course+" AND dept='"+dept+"' AND department_id=departments.id;")
-	return str(cur.fetchone()[0])
+	cur.execute("SELECT id FROM allcourses WHERE number="+course+" AND department_id="+str(dept)+";")
+	id=cur.fetchone()
+	if(id):
+		return id[0]
+	else:
+		cur.execute("INSERT INTO allcourses (number,department_id) VALUES( "+course+", "+str(dept)+" );")
+		cur.execute("SELECT LAST_INSERT_ID();")
+		id=cur.fetchone()[0]
+		print "Adding new course: "+course+" with id: "+str(id)
+		return id
 def get_prof_id(dept,prof):
 	cur.execute("SELECT id FROM profs WHERE department_id='"+str(dept)+"' AND display_name='"+MySQLdb.escape_string(prof)+"';")	
 	id=cur.fetchone()
@@ -30,13 +38,13 @@ def get_prof_id(dept,prof):
 		cur.execute("INSERT INTO profs (department_id,display_name) VALUES( "+str(dept)+", '"+MySQLdb.escape_string(prof)+"');")
 		cur.execute("SELECT LAST_INSERT_ID();")
 		return cur.fetchone()[0]
-def insert_grade(department_id,prof_id,course_number,section_number,A,B,C,D,F,TOT):
-	cur.execute("INSERT INTO grades (department_id,prof_id,course_number,section_number,A,B,C,D,F,TOT) VALUES "+\
-		"("+department_id+", "+prof_id+", "+course_number+", "+section_number+", "+A+", "+B+", "+C+", "+D+", "+F+", "+TOT+");")
+def insert_grade(department_id,prof_id,course_id,A,B,C,D,F,TOT):
+	cur.execute("INSERT INTO grades (department_id,prof_id,course_id,A,B,C,D,F,TOT) VALUES "+\
+		"("+department_id+", "+prof_id+", "+course_id+", "+A+", "+B+", "+C+", "+D+", "+F+", "+TOT+");")
 fields={}
 fields["department_id"]=(0,4)
-fields["course_number"]=(5,3)
-fields["section_number"]=(9,3)
+fields["course_id"]=(5,3)
+#fields["section_number"]=(9,3)
 fields["A"]=(18,7)
 fields["B"]=(26,7)
 fields["C"]=(34,7)
@@ -54,7 +62,8 @@ for line in sys.stdin:
 	if(values["prof_id"]=='0'):
 		continue
 	values["department_id"]=get_dept_id(values["department_id"])
-	values["prof_id"]=get_prof_id(values["department_id"],values["prof_id"])
+	values["course_id"]=  get_course_id(values["department_id"],values["course_id"])
+	values["prof_id"]=      get_prof_id(values["department_id"],values["prof_id"])
 	for f in fields:
 			values[f]=str(int(values[f]))
 	if(values['TOT']=='0'):
